d755d99b3f1ab356934eb377b34fa07ada085cb0
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-06-12 11:44:26 +0545

    Time: 84 ms (15.50%), Space: 10.9 MB (48.90%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+        vector<vector<int>> final;
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+           vector<int> temp;
+    combinationSumHelper(candidates, 0, target, temp);
+        return final;
+    }
+void combinationSumHelper(vector<int> &candidates, int n, int target, vector<int> &temp)
+{
+    // cout<<candidates.size()-1<<endl;
+    if (n == candidates.size())
+    {
+        if (target == 0)
+        {
+            // cout<<"hello"<<endl;
+            // for (int i = 0; i < temp.size(); i++)
+            //     {cout << temp.at(i);}
+            final.push_back(temp);
+        }
+
+        return;
+    }
+    cout<<target<<endl;
+    // every element has two option to be included ot nor
+
+if (candidates[n]<=target)
+{
+    temp.push_back(candidates[n]);
+    combinationSumHelper(candidates, n, target - candidates[n], temp);
+    temp.pop_back();
+}
+    combinationSumHelper(candidates, n + 1, target, temp);
+    return;
+}
+};
