82490b7c31214f6b8184c33d69533c277aa88bd8
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-06-19 00:27:28 +0545

    Time: 5 ms (21.49%), Space: 6.4 MB (50.93%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int helper(int i, int j, vector<vector<int>> &dp)
+    {
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+         if(i<0 || j<0)
+    return 0;
+        // base case is out of bounds
+        if (i == 0 && j == 0)
+        {
+            return 1;
+        }
+        int up = 0, left = 0;
+        if (i - 1 >= 0)
+            up = helper(i - 1, j, dp);
+        if (j - 1 >= 0)
+            left = helper(i, j - 1, dp);
+
+        return dp[i][j] = up + left;
+    }
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return helper(m - 1, n - 1, dp);
+    }
+};
