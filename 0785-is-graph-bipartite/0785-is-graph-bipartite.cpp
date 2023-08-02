48eb18ef30fc2336f71af7980e871fda6f6254c5
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-08-02 13:54:10 +0530

    Time: 19 ms (95.67%), Space: 13.5 MB (48.90%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool dfs(int clr,int i ,vector<vector<int>>& graph,int color[] ){
+        // cout<<i<<" "<<clr<<endl;
+        color[i]=clr;
+        for(auto adj:graph[i]){
+            if(color[adj]==-1) {if(!dfs(!clr,adj,graph,color)){return false;}}
+            else if(color[adj]==clr) {return false;}
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        
+        //well use dfs
+        //bipartitle means no cycle or two adjacent doest have sam
+        int n = graph.size();
+        	    int color[n];
+
+	    for(int i = 0;i<n;i++) color[i] = -1; 
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if (dfs(0,i,graph,color)==false) return false;
+            }
+        }
+        return true;
+    }
+};
