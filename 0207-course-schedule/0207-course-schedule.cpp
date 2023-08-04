56908faee21da0934dccd535619b4806980ed433
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-08-04 13:07:24 +0530

    Time: 12 ms (99.15%), Space: 13.4 MB (78.53%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+    bool canFinish(int nodes, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(nodes );
+            vector<int> indeg(nodes , 0);
+
+
+    for (int i = 0; i < prerequisites.size(); i++)
+    {
+        adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+         // indeg[prerequisites[i][0]]++;
+    }
+
+
+    for (int i = 0; i < nodes; i++)
+    {
+
+        for (auto it : adj[i])
+        {
+            indeg[it]++;
+        }
+    }
+
+
+queue<int> q;
+vector<int> ans;
+
+for(int i = 0; i < nodes; i++)
+{
+    if (indeg[i] == 0){
+        q.push(i);
+        ans.push_back(i);}
+}
+
+
+while (!q.empty())
+{
+
+    int node = q.front();
+    q.pop();
+
+    for (auto it : adj[node])
+    {
+        indeg[it]--;
+        if (indeg[it] == 0)
+        {
+            ans.push_back(it);
+
+            q.push(it);
+        }
+    }
+}
+return ans.size()==nodes;
+    }
+};
