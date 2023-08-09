d39dac467c2c195610c6f8013be6ea1cca25dbb0
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-08-09 13:05:18 +0530

    Time: 96 ms (99.09%), Space: 19.9 MB (61.77%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int rows = heights.size();
+        int cols = heights[0].size();
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
+
+        dist[0][0] = 0;
+        pq.push({0, {0, 0}});
+
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, -1, 0, 1};
+
+        while (!pq.empty()) {
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            int dis = pq.top().first;
+
+            if (x == rows - 1 && y == cols - 1)
+                return dis;
+
+            pq.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + delrow[i];
+                int ny = y + delcol[i];
+
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols) {
+                    int new_effort = max(dis, abs(heights[nx][ny] - heights[x][y])); //IMP , PATHS NEW EFOOR IS MAX OF (,) NOT JUST THE ABS DIFF 
+
+                    if (dist[nx][ny] > new_effort) {
+                        dist[nx][ny] = new_effort;
+                        pq.push({new_effort, {nx, ny}});
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+};
