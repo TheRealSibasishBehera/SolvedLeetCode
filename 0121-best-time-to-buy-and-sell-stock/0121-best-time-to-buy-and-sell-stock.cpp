3d73bc26be6d41ef1b02107edffbffed6975fbde
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-15 22:34:19 +0530

    Time: 96 ms (57.15%), Space: 93.6 MB (93.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
 int maxProfit(vector<int> &prices)
 {
-
-
-    //here is basic sliding window theory
-    //we need to find the max diffrence between the elements
-    // 7 1 5 3 6 4
+    // here is basic sliding window theory
+    // we need to find the max diffrence between the elements
+    //  7 1 5 3 6 4
     int l = 0;
     int r = 1;
     int n = prices.size();
     int ans = 0;
-    while(r<n){
-        int val = prices[r]-prices[l];
-        ans = max(ans,val);
-        if(prices[l]>prices[r]){
-            l=r;
-            r++;
-        }else{
-            r++;
+    while (r < n)
+    {
+        int val = prices[r] - prices[l];
+        ans = max(ans, val);
+        if (prices[l] > prices[r])
+        {
+            l = r;
         }
+        r++;
     }
-
-
 
     return ans;
 }
