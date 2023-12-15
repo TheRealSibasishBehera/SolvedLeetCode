c658256b2ddf4fc830b196d29be9ad218b580b7a
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-15 20:46:43 +0530

    Time: 8 ms (65.12%), Space: 15.9 MB (93.96%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-            vector<pair<int, int>> v;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        v.push_back({nums[i], i});
-    }
-    // sort(v.begin(), v.end()); // since by default it checks first element of pair
-    // sort(nums.begin(), nums.end());
+    vector<int> twoSum(vector<int>& v, int target) {
+    
+    // vector<pair<int, int>> v;
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+        // v.push_back({nums[i], i});
+    // }
     int i = 0;
     int j = v.size() - 1;
     while (i < j)
     {
-        if (v[i].first + v[j].first == target)
-            return {v[i].second+1, v[j].second+1};
-        else if (v[i].first + v[j].first > target)
+        if (v[i] + v[j] == target)
+            return {i+1, j+1};
+        else if (v[i]+ v[j] > target)
             j--;
         else
             i++;
