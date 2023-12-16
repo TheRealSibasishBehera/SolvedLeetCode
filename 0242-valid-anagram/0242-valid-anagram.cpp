ac811b366775f7b68cf13a23f92643b7705b209b
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-16 07:44:54 +0530

    Time: 10 ms (52.26%), Space: 7.8 MB (45.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,11 +2,22 @@ class Solution {
 public:
 bool isAnagram(string s, string t)
 {
+    
+               if (s.size() != t.size())
+        return false;
     //MAKE 2 SET AND CHECK IF EQUAL
     // OR MAKE 1 SET AND CHECK IF SIZE IS 0 IF s++ t--;
-    sort(s.begin(),s.end());
-    sort(t.begin(),t.end());
-    if(s==t) return true;
-    return false;
+
+    unordered_map<char,int> mp;
+    for (int i = 0; i < s.size(); i++)
+    {
+        mp[s[i]]++;
+        mp[t[i]]--;
+    }
+
+    for(auto x:mp){
+        if(x.second!=0) return false;
+    }
+    return true;    
 }
 };
