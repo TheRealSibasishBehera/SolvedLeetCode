798b22a68dc59996203ecee489cafe77d845dcad
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-16 16:36:06 +0530

    Time: 6 ms (87.15%), Space: 12.4 MB (39.38%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -5,33 +5,39 @@ using namespace std;
 
 class Solution {
 public:
-    int evalRPN(vector<string> &tokens) {
-        stack<int> st;
-        for (const string &token : tokens) {
-            if (isOperator(token)) {
-                int x = st.top();
-                st.pop();
-                int y = st.top();
-                st.pop();
-
-                if (token == "+") {
-                    st.push(y + x);
-                } else if (token == "-") {
-                    st.push(y - x);
-                } else if (token == "*") {
-                    st.push(y * x);
-                } else if (token == "/") {
-                    if (x != 0) {
-                        st.push(y / x);
-                    } else {
-                        continue;
-                    }
+  int evalRPN(vector<string>& tokens) {
+        stack<int> rpn; 
+        for(int i = 0; i < tokens.size(); i++){
+            string segment = tokens[i]; 
+            
+            if(segment == "+"|| segment == "-" || segment == "*" || segment =="/"){
+                int val1 = rpn.top();
+                rpn.pop();
+                int val2 = rpn.top();
+                rpn.pop();
+                int new_val = 0;
+                if(segment == "+"){
+                    new_val = val1 + val2;
                 }
-            } else {
-                st.push(stoi(token));
+                else if(segment == "-"){
+                    new_val = val2 - val1;
+                }
+                else if(segment == "*"){
+                    new_val = val1 * val2;
+                }
+                else if(segment == "/"){
+                    new_val = floor(val2/val1);
+                }
+                rpn.push(new_val);
+                
+            }
+            else{
+                int val = stoi(segment);
+                rpn.push(val);
             }
         }
-        return st.top(); // Return the final result
+        return rpn.top();
+        
     }
 
 private:
