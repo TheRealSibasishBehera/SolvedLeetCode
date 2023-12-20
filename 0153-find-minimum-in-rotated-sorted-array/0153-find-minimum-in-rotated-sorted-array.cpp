57f491601dbc1a44d36e6836819c9c492f697f24
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-20 22:50:09 +0530

    Time: 7 ms (8.31%), Space: 10.4 MB (67.10%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-int findMin(vector<int> &nums)
-{
+int findMin(vector<int> &nums){
     int start = 0;
     int end = nums.size() - 1;
-    int ans=INT_MAX;
-    while (start<=end)
-    {
-        int mid = start + (end-start)/2;
+    int ans = INT_MAX;
 
+        // basically one part has to be sorted
+        //2 part can be sorted once we enter sorted section 
+        // so we do one thing , we keep min of sorted
+        // and then we move to unsorted section
+        // do the same there , divide into unsorted and sorted
+        // and keep min of sorted
 
+        while(start<=end){
+            int mid = start + (end-start)/2;
 
-        //when u know for sure
-        //that the array is sorted
-        //then u can directly return the start
-        //as the min element
-        if(nums[start]<=nums[end])
-        {
-            ans = min(ans,nums[start]);
-            // return ans;
-            break;
+            //full array is sorted
+            //we can end now
+            if(nums[start]<=nums[end]){
+                ans = min(ans,nums[start]);
+                break;
+            }
+
+            //left is sorted
+            else if(nums[start]<=nums[mid]){
+                ans= min(ans,nums[start]);
+                start=mid+1;
+            }
+
+            //left is unsorted
+            else if(nums[mid]<=nums[end]){
+                ans=min(ans,nums[mid]);
+                end=mid-1;
+            }
         }
-        //FIRST HALD IS SORTED
-        if(nums[start]<=nums[mid])
-        {
-            ans= min(ans,nums[start]);
-            start=mid+1;
-        }
 
-        //SECOND HALF IS SORTED
-        else if(nums[mid]<=nums[end])
-        {
-            ans=min(ans,nums[mid]);
-            end=mid-1;
-        }
-
-
-
+        return ans;
     }
-
-    return ans;
-}
 };
