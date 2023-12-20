b0c88aa26a1bdfba23d1a586a15f5555e84e7871
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-20 23:45:45 +0530

    Time: 194 ms (5.14%), Space: 119.2 MB (13.55%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
-bool check(vector<int> &nums,int m,int k,int mid){
-    int bouq = 0;
-    int flowers = 0;
-
-
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]<=mid){
-            flowers++;
-            if(flowers==k){
-                bouq++;
-                flowers=0;
+    bool check2(vector<int> &nums, int m, int k, int mid) {
+        vector<int> temp = nums;
+        int flowers = 0;
+        int bouq = 0;
+        
+        for (int i = 0; i < temp.size(); i++) {
+            int newe = nums[i] - mid;
+            if (newe <= 0) {
+                temp[i] = 0;
             }
         }
-        else{
-            flowers=0;
-        }
-    }
-    return bouq>=m;
-}
-int minDays(vector<int> &nums, int m, int k)
-{
-    long long int val = static_cast<long long int>(m) * k; // Ensure the multiplication doesn't overflow
-    if (nums.size() < val) return -1;
-    int start = 0;
-    long long int end = *max_element(nums.begin(), nums.end()); // Change the type to long long int
-    while (start <= end) {
-        long long int mid = start + (end - start) / 2;
-        if (check(nums, m, k, mid)) {
-            end = mid - 1;
-        } else {
-            start = mid + 1;
-        }
-    }
-    return static_cast<int>(end + 1);
-}
 
+        for (int i = 0; i < temp.size(); i++) {
+            if (temp[i] == 0) {
+                flowers++;
+                if (flowers == k) {
+                    bouq++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+        return bouq >= m;
+    }
+
+    int minDays(vector<int> &nums, int m, int k) {
+        long long int si = nums.size();
+        long long int mcrk = static_cast<long long int>(m) * k;
+        if (si < mcrk) return -1;
+
+        int start = 0;
+        int end = *max_element(nums.begin(), nums.end());
+        int ans = INT_MAX;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (check2(nums, m, k, mid)) {
+                ans = min(ans, mid);
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans == INT_MAX ? -1 : ans;
+    }
 };
