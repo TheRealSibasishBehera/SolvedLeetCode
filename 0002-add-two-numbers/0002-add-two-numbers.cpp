420b4fe20d479ecedcb803c434bd78802f9c3a39
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-24 02:00:15 +0530

    Time: 32 ms (12.43%), Space: 71.9 MB (25.31%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,73 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-{
-    ListNode *head1 = l1;
-    ListNode *head2 = l2;
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *head1 = l1;
+        ListNode *head2 = l2;
 
-    ListNode *temp=new ListNode();
-    ListNode *dummy = temp;
+        ListNode *dummy = new ListNode();
+        ListNode *temp = dummy;
 
-    if (!l1 && !l2)
-        return nullptr;
-    int remain = 0;
-    while(head1 && head2){
-        int ans = head1->val + head2->val + remain;
+        int remain = 0;
 
-        remain = ans / 10;
-        ans = ans % 10;
+        while (head1 || head2 || remain) {
+            int sum = remain;
 
-        ListNode *temp2 = new ListNode(ans);
-        temp->next = temp2;
-        temp = temp->next;
-            head2=head2->next;
-            head1=head1->next;
-        
-        
-    }
-    if(head1){
-        while(head1){
-            int ans = head1->val + remain;
-        remain = ans / 10;
-        ans = ans % 10;
-            ListNode *temp2 = new ListNode(ans);
-            temp->next = temp2;
+            if (head1) {
+                sum += head1->val;
+                head1 = head1->next;
+            }
+
+            if (head2) {
+                sum += head2->val;
+                head2 = head2->next;
+            }
+
+            remain = sum / 10;
+            temp->next = new ListNode(sum % 10);
             temp = temp->next;
-            head1=head1->next;
-            
         }
-        
+
+        return dummy->next;
     }
-
-    if(head2){
-        while(head2){
-            int ans = head2->val + remain;
-        remain = ans / 10;
-        ans = ans % 10;
-            ListNode *temp2 = new ListNode(ans);
-            temp->next = temp2;
-            temp = temp->next;
-            head2=head2->next;
-        }
-        
-    }
-
-    if(remain!=0){
-        ListNode *temp2 = new ListNode(remain);
-        temp->next = temp2;
-        temp = temp->next;
-    }   
-
-    return dummy->next;
-}
 };
