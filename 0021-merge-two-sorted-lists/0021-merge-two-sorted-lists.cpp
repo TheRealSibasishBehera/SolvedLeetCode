7d7d1d51617c3972f68807b9822752c9f0e1c51e
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-24 00:42:38 +0530

    Time: 4 ms (55.86%), Space: 15.4 MB (11.00%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,67 +10,64 @@
  */
 class Solution {
 public:
-
-ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
-{
-    ListNode *head1 = list1;
-    ListNode *head2 = list2;
-
-    
-    if(!list1 && !list2) return nullptr;
-    if(!list1 ) return list2;
-    if(!list2) return list1;
-    // initiasise
-    ListNode *temp;
-    ListNode *dummy;
-
-    int big = 0;
-
-    if (head1->val > head2->val)
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
     {
-        temp = new ListNode(head2->val);
-        head2 = head2->next;
-        big = 2;
-    }
-    else
-    {
-        temp = new ListNode(head1->val);
-        head1 = head1->next;
-        big = 1;
-    }
+        ListNode *head1 = list1;
+        ListNode *head2 = list2;
 
-    dummy = temp;
-    while (head1 && head2)
-    {
+        if (!list1 && !list2)
+            return nullptr;
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
+        // initiasise
+        ListNode *temp;
+        ListNode *dummy;
+
+        int big = 0;
+
         if (head1->val > head2->val)
         {
-            ListNode *tempo = new ListNode(head2->val);
-            dummy->next = tempo;
-            dummy = dummy->next;
+            temp = new ListNode(head2->val);
             head2 = head2->next;
-        }else{
-            ListNode *tempo = new ListNode(head1->val);
-            dummy->next = tempo;
-            dummy = dummy->next;
-            head1 = head1->next;
+            big = 2;
         }
+        else
+        {
+            temp = new ListNode(head1->val);
+            head1 = head1->next;
+            big = 1;
+        }
+
+        dummy = temp;
+        while (head1 && head2)
+        {
+            if (head1->val > head2->val)
+            {
+                ListNode *tempo = new ListNode(head2->val);
+                dummy->next = tempo;
+                dummy = dummy->next;
+                head2 = head2->next;
+            }
+            else
+            {
+                ListNode *tempo = new ListNode(head1->val);
+                dummy->next = tempo;
+                dummy = dummy->next;
+                head1 = head1->next;
+            }
+        }
+
+        if (head1)
+        {
+            dummy->next =head1;
+        }
+        if (head2)
+        {
+            dummy->next = head2;
+        }
+
+        return temp;
     }
-
-    while(head1){
-        ListNode *tempo = new ListNode(head1->val);
-        dummy->next = tempo;
-        dummy = dummy->next;
-        head1 = head1->next;
-    }
-    while(head2){
-        ListNode *tempo = new ListNode(head2->val);
-        dummy->next = tempo;
-        dummy = dummy->next;
-        head2 = head2->next;
-    }   
-
-
-
-    return temp;
-}
 };
