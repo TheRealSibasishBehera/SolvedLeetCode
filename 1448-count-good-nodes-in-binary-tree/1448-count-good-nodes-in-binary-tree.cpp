1668e631f88431be83b58e34131ca194d4c6461e
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-25 01:03:31 +0530

    Time: 98 ms (77.10%), Space: 86.9 MB (11.34%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,36 +11,23 @@
  */
 class Solution {
 public:
-    int counti = 0; // Initialize counti to 0
-    int maxi = INT_MIN; // Initialize maxi to the minimum possible integer value
-
-    void helper(TreeNode* node) {
-        if (!node)
-            return;
-
-        int x = node->val;
-        if (x >= maxi)
-            counti++;
-
-        maxi = max(x, maxi);
-        int prev = maxi;
-
-        helper(node->right);
-
-        // restore maxi value;
-        maxi = prev;
-
-        helper(node->left);
-        // return counti a+b;
+    int goodNodes(TreeNode* root) {
+        return helper(root, INT_MIN);
     }
 
-    int goodNodes(TreeNode* root) {
-        // looks like a dfs question
-        if (!root)
+private:
+    int helper(TreeNode* node, int maxi) {
+        if (!node)
             return 0;
 
-        int maxi = root->val; // Initialize maxi with the value of the root node
-        helper(root);
-        return counti;
+        int x = node->val;
+        int counti = (x >= maxi) ? 1 : 0;
+
+        maxi = std::max(x, maxi);
+
+        int countLeft = helper(node->left, maxi);
+        int countRight = helper(node->right, maxi);
+
+        return counti + countLeft + countRight;
     }
 };
