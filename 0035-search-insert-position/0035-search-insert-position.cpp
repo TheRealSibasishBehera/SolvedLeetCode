5c36031da8bbaf41ad7da7fc01b2e8b73465ddf2
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-26 00:23:19 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (98.24%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
-int searchInsert(vector<int> &arr, int target)
+int searchInsert(vector<int> &arr, int x)
 {
 
-    //we know that lowerbound give iterator to the element or just greater
-    //perfect
-    int x = lower_bound(arr.begin(),arr.end(),target)-arr.begin();
-    return x;
+    // we know that lowerbound give iterator to the element or just greater
+    // perfect
+    int n = arr.size();
+
+    // we know that lowerbound give iterator to the element or just greater
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x)
+        {
+            ans = mid;
+
+            //KEY
+            // look for smaller index on the left
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
 }
 };
