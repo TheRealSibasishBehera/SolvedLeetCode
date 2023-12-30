74e5a0705ca0f18cb2d5299cc919384587fba4f4
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-30 16:58:09 +0530

    Time: 19 ms (11.12%), Space: 9.4 MB (45.59%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<int> parent(1001, -1);
-        vector<int> rank(1001, 0);
+vector<int> findRedundantConnection(vector<vector<int>> &edges)
+{
+    vector<int> rank(1001, -1);
+    vector<int> parent(1001, -1); // Initialize with -1
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int a = edges[i][0];
+        int b = edges[i][1];
 
-        for (int i = 0; i < edges.size(); i++) {
-            int x = edges[i][0];
-            int y = edges[i][1];
-
-            int parentX = findParent(x, parent);
-            int parentY = findParent(y, parent);
-
-            if (parentX == parentY) {
-                return edges[i];
-            }
-
-            // Union by rank
-            if (rank[parentX] > rank[parentY]) {
-                parent[parentY] = parentX;
-            } else if (rank[parentX] < rank[parentY]) {
-                parent[parentX] = parentY;
-            } else {
-                parent[parentY] = parentX;
-                rank[parentX]++;
-            }
-        }
-
-        return {};
+        if (!union_nodes(a, b, rank, parent))
+            return {a, b};
     }
 
-private:
-    int findParent(int node, vector<int>& parent) {
-        if (parent[node] == -1) {
-            return node;
-        }
-        return findParent(parent[node], parent);
+    return {};
+}
+
+int find_par(int x, vector<int> &parent)
+{
+    if (parent[x] == -1)
+        return x;
+    else
+        return find_par(parent[x], parent);
+}
+
+bool union_nodes(int a, int b, vector<int> &rank, vector<int> &parent)
+{
+    // Base case: if same parent, it will be a redundant connection
+    int parA = find_par(a, parent);
+    int parB = find_par(b, parent);
+    if (parA == parB)
+        return false;
+
+    // Union by rank
+    if (rank[parA] > rank[parB])
+    {
+        // Merge b to a
+        parent[parB] = parA;
+        rank[parA] += rank[parB];
     }
+    else
+    {
+        parent[parA] = parB;
+        rank[parB] += rank[parA];
+    }
+
+    return true;
+}
+
 };
