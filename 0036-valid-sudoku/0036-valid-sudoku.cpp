c261841a3a62e4b822310446009d7c46afeec377
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2023-12-31 16:48:12 +0530

    Time: 47 ms (5.22%), Space: 18.4 MB (78.70%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
     
-    
 bool isValidSudoku(vector<vector<char>> &board)
 {
-    //we will use 3 hashmaps
-    //one for row
-    //one for col
-    //one for 3*3 matrix
-    unordered_set<int> row[9];
-    unordered_set<int> col[9];
-    unordered_set<int> matrix[3][3];
+    // We will use arrays instead of unordered_set
+    // One array for each row, column, and 3*3 matrix
+    int row[9][10] = {0};  // 10-sized array to track the occurrence of digits 1-9
+    int col[9][10] = {0};
+    int matrix[3][3][10] = {0};
 
     for (int i = 0; i < board.size(); i++)
     {
         for (int j = 0; j < board[0].size(); j++)
         {
-            if(board[i][j] == '.')
+            if (board[i][j] == '.')
                 continue;
+
             int curr = board[i][j] - '0';
-            if (row[i].find(curr) != row[i].end())
-                return false;
-            if (col[j].find(curr) != col[j].end())
-                return false;
 
-            //this is the tricky part
-            //we have to find the matrix number
-            //it belongs to 
-            // its always i/3 and j/3
-
-            if(matrix[(i/3)][j/3].find(curr) != matrix[(i/3)][j/3].end())
+            // Check if the current digit already exists in the corresponding row, column, or matrix
+            if (row[i][curr] == 1)
+                return false;
+            if (col[j][curr] == 1)
+                return false;
+            if (matrix[i / 3][j / 3][curr] == 1)
                 return false;
 
-            row[i].insert(curr);
-            col[j].insert(curr);
-            matrix[i/3][j/3].insert(curr);  
+            // Mark the current digit as seen in the row, column, and matrix
+            row[i][curr] = 1;
+            col[j][curr] = 1;
+            matrix[i / 3][j / 3][curr] = 1;
         }
-        
     }
 
     return true;
-    
 }
 };
