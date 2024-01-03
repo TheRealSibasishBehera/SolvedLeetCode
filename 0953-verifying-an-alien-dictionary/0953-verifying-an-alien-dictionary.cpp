ea5635c71036dee64b6ddbc1918b4a5bd30c420e
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-03 20:48:12 +0530

    Time: 3 ms (82.38%), Space: 10.1 MB (26.69%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -8,16 +8,16 @@ public:
         for (int i = 0; i < words.size() - 1; i++)
         {
             int j = 0;
-            int flag=0;
-            while (j < words[i].size() && j < words[i + 1].size())
-            {
-                if (order.find(words[i][j]) > order.find(words[i + 1][j])) return false;
-                if (order.find(words[i][j]) == order.find(words[i + 1][j]))
-                    j++;
-                else {flag=1;break;}
+            int flag = 0;
+            for(int j = 0; j < words[i].size(); j++){
+                if(j==words[i+1].size()) return false;
+                
+                //find the first mismatch
+                if(words[i][j]!=words[i+1][j]){
+                    if(order.find(words[i][j])>order.find(words[i+1][j])) return false;
+                    else break;
+                }
             }
-            
-            if(!flag && words[i].size() > words[i+1].size()) return false;
         }
         return true;
     }
