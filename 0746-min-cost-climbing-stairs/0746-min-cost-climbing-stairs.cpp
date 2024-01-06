0d6cdda99ab057ec0d620d77c220c29101a919cf
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-06 12:31:19 +0530

    Time: 0 ms (100.00%), Space: 13.9 MB (84.48%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -4,15 +4,18 @@ public:
     int minCostClimbingStairs(vector<int> &cost)
     {
         int sizee = cost.size();
-        vector<int> dp(sizee + 2, -1);
-        // we will do it using iteratively
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        // Initialize x and y with the costs of the first two steps
+        int x = cost[0];
+        int y = cost[1];
+        
         for (int i = 2; i < sizee; i++)
         {
-            dp[i]  = cost[i] + min(dp[i-1], dp[i-2]);
+            int curr = cost[i] + min(x, y);
+            x = y;
+            y = curr;
         }
 
-        return min(dp[sizee-1], dp[sizee-2]);
+        return min(x, y);
+        // The minimum cost to reach the top is either from the last step or the second-to-last step
     }
 };
