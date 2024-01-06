99cdc70e0d7825a710f15c0bb9496476cf434926
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-06 12:06:52 +0530

    Time: 2 ms (45.03%), Space: 6.3 MB (70.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,6 +3,8 @@ class Solution
 public:
     int climbStairs(int n)
     {
+                if(n==1){return 1;}
+        if(n==0){return 1;}
         int prev = 0;
         int prev2 = 1;
         int curr = 0;
