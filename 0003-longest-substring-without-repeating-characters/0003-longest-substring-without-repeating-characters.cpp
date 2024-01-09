e3dbf822a785bbe3cf131a02896f9d90585259be
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-09 17:13:16 +0530

    Time: 12 ms (66.85%), Space: 8.8 MB (56.68%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,23 +3,27 @@ class Solution
 public:
     int lengthOfLongestSubstring(string s)
     {
-        if (s.size() == 0) return 0;
+        if (s.size() == 0)
+            return 0;
+
         int start = 0;
         int n = s.size();
         int maxi = INT_MIN;
-        unordered_map<char, int> sete;  // Use unordered_map to store characters and their indices
-        for (int i = 0; i < n; i++)
-        {
-            if (sete.find(s[i]) != sete.end())
-            {
-                maxi = max(maxi, i - start);
-                start = max(start, sete[s[i]] + 1);
-            }
-            sete[s[i]] = i;  // Store character and its index in the map
-        }
+        unordered_map<char, int> sete; // Use unordered_map to store characters and their indices
 
-        // Check for the last substring
-        maxi = max(maxi, n - start);
+        int l = 0, r = 0;
+        while (r < s.size())
+        {
+            // check the element
+            if (sete.find(s[r]) != sete.end())
+            {
+                l = max(sete[s[r]] + 1, l); // Update l only if it's greater than the current l
+            }
+
+            sete[s[r]] = r;
+            r++;
+            maxi = max(maxi, r - l); // Update maxi here to consider the current substring length
+        }
 
         return maxi;
     }
