d0260ac5bc4e3e4f350bcb313cca2c8b913d6a63
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-09 16:43:34 +0530

    Time: 92 ms (76.42%), Space: 93.7 MB (59.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-int maxProfit(vector<int> &prices)
-{
-    int n = prices.size();
-    //try to max diffrence 
-    int min_price = INT_MAX;
-    int ans=0;
-    for (int i = 0; i < n; i++)
-    {
-        int diff= prices[i]-min_price;
-        ans=max(ans,diff);
-        if(diff<0) {
-            min_price=prices[i];
+    int maxProfit(vector<int>& prices) {
+        int pivot = prices[0] ;
+        int maxi = INT_MIN;
+        for(int i =0 ; i< prices.size();i++){
+            maxi= max(maxi , prices[i] - pivot);
+            if(prices[i]<pivot) pivot = prices[i];
         }
+        
+        
+        return maxi;
     }
-
-    return ans;
-    
-}
 };
