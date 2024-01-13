7ffe76f34a7ba2a5e74fb10c1156ec4e5e112802
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-13 22:51:02 +0530

    Time: 3 ms (33.26%), Space: 7 MB (10.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,51 +1,29 @@
 class Solution
 {
 public:
-    int helper(int i, int j, vector<vector<int>> &dp)
+    int helper(int i, int j, int m, int n, vector<vector<int>> &dp)
     {
+        // base case
+        if (i == 0 && j == 0)
+            return 1;
 
+        // could not be reached
+        if (i < 0 || j < 0)
+            return 0;
+
+        // check if already calculated
         if (dp[i][j] != -1)
             return dp[i][j];
 
-         if(i<0 || j<0)
-    return 0;
-        // base case is out of bounds
-        if (i == 0 && j == 0)
-        {
-            return 1;
-        }
-        int up = 0, left = 0;
-        if (i - 1 >= 0)
-            up = helper(i - 1, j, dp);
-        if (j - 1 >= 0)
-            left = helper(i, j - 1, dp);
-
-        return dp[i][j] = up + left;
+        int x = 0;
+        int y = 0;
+        x = helper(i, j - 1, m, n, dp);
+        y = helper(i - 1, j, m, n, dp);
+        return dp[i][j] = x + y;
     }
     int uniquePaths(int m, int n)
     {
-    vector<vector<int> > dp(m,vector<int>(n,-1));
- for(int i=0; i<m ;i++){
-      for(int j=0; j<n; j++){
-          
-          //base condition
-          if(i==0 && j==0){
-              dp[i][j]=1;
-              continue;
-          }
-          
-          int up=0;
-          int left = 0;
-          
-          if(i>0) 
-            up = dp[i-1][j];
-          if(j>0)
-            left = dp[i][j-1];
-            
-          dp[i][j] = up+left;
-      }
-  }
-  
-  return dp[m-1][n-1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m - 1, n - 1, m, n, dp);
     }
 };
