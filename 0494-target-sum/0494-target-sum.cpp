d71885067ffddb5f72ca7bf9cb2021e3b1610a25
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-13 16:50:20 +0530

    Time: 322 ms (35.67%), Space: 62.7 MB (15.38%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,33 +1,34 @@
-class Solution
-{
+/*
+    Given int array & a target, want to build expressions w/ '+' & '-'
+    Return number of different expressions that evaluates to target
+
+    Recursion w/ memoization, cache on (index, total), which stores # ways
+    If total ever reaches the target, return 1 (this is a way), else 0
+
+    Time: O(n x target)
+    Space: O(n x target)
+*/
+
+class Solution {
 public:
-    int offset = 1001;
-    map<pair<int, int>, int> dp;
-
-    int helper(int target, vector<int> &nums, int index, int count)
-    {
-        // base case
-        if (index < 0)
-        {
-            return (target == 0) ? 1 : 0;
-        }
-
-        if (dp.find({index, target}) != dp.end())
-        {
-            return dp[{index, target}];
-        };
-        // either u can add or subtract
-        int x = 0,
-            y = 0;
-        x = helper(target - nums[index], nums, index - 1, count);
-        y = helper(target + nums[index], nums, index - 1, count);
-
-        return dp[{index, target}] = x + y;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return backtrack(nums, target, 0, 0);
     }
-    int findTargetSumWays(vector<int> &nums, int target)
-    {
-        // meed to include negative numbers
-        // vector<vector<int>> dp(offset * 2, vector<int>(nums.size() + 1, -1));
-        return helper(target, nums, nums.size() - 1, 0);
+private:
+    // {(index, total) -> # of ways}
+    map<pair<int, int>, int> dp;
+    
+    int backtrack(vector<int>& nums, int target, int i, int total) {
+        if (i == nums.size()) {
+            return total == target ? 1 : 0;
+        }
+        if (dp.find({i, total}) != dp.end()) {
+            return dp[{i, total}];
+        }
+        
+        dp[{i, total}] = backtrack(nums, target, i + 1, total + nums[i])
+                       + backtrack(nums, target, i + 1, total - nums[i]);
+        
+        return dp[{i, total}];
     }
 };
