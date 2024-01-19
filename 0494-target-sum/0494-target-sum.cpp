7ab0dd2136f003c9c8b7d0fc555971083ab5213a
Sibasish Behera <95071627+TheRealSibasishBehera@users.noreply.github.com>
2024-01-19 12:11:40 +0530

    Time: 10 ms (80.03%), Space: 12.7 MB (42.20%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,39 @@
-class Solution
-{
+class Solution {
 public:
-    int helper(int n, int target, vector<int> &nums, vector<unordered_map<int, int>> &dp)
-    {
-        if (n == 0)
-        {
-            if (target == 0)
-                return 1;
+    int helper(int n, int k, vector<int> &arr, vector<vector<int>> &dp) {
+        // base condition
+        if (n == 1) {
+            if (k == 0 && arr[0] == 0) return 2;
+            if (k == 0 || k == arr[0]) return 1;
             return 0;
         }
-        if (dp[n].find(target) != dp[n].end())
-            return dp[n][target];
-        int add = 0, sub = 0;
-        add = helper(n - 1, target + nums[n - 1], nums,dp);
-        sub = helper(n - 1, target - nums[n - 1], nums,dp);
-        return dp[n][target] = add + sub;
+        if (n == 0) {
+            return (k == 0) ? 1 : 0;
+        }
+
+        if (dp[n][k] != -1)
+            return dp[n][k];
+        int pick = 0, not_pick = 0;
+        if (k - arr[n - 1] >= 0)
+            pick = helper(n - 1, k - arr[n - 1], arr, dp);
+        not_pick = helper(n - 1, k, arr, dp);
+
+        return dp[n][k] = pick + not_pick;
     }
-    int findTargetSumWays(vector<int> &nums, int target)
-    {
+
+    int findTargetSumWays(vector<int> &nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        // If the sum is odd or target is too large, there is no way to achieve the target.
+        if (sum < target || (sum + target) % 2 != 0) {
+            return 0;
+        }
+
+        int k = (sum + target) / 2;
+            if(k<0) return 0;
+
         int n = nums.size();
-        vector<unordered_map<int, int>> dp(n + 1);
-        return helper(n, target, nums, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
+        return helper(n, k, nums, dp);
     }
 };
